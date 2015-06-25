ce64b4ed030efb481c3dc84e1adb56ef4f25ec8b
flyingfisch <flyingfisch@toppagedesign.com>
2015-06-24 21:49:05 -0400

    after a bit of error handling we should be good to go!

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
 #!/usr/bin/python3
 
+import sys
+
+# Put the command line input in a var
+input = sys.argv[1]
+
 numerals = {
     'I': 1,
     'V': 5,
@@ -9,25 +14,34 @@ numerals = {
     'M': 1000,
 }
 
-#test
 def parseRoman(text):
     result = 0
     counter = 0
 
+    # cycle through the digits in the roman numeral
     for i in text:
+        # Get the value of the roman numeral
         value = numerals[i]
 
+        # if counter + 1 is less than the length of the string, check the next digit to decide whether to subtract this number from the total or to add it
         if counter + 1 < len(text):
             next = numerals[text[counter + 1]]
         else:
             next = numerals[text[counter]]
+
         operator = -1 if next > value else 1
 
+        # increment counter, add the value of this digit to the total
         counter += 1
         result += operator * value
 
     return result
 
-print(parseRoman("IV"))
-print(parseRoman("XX"))
-print(parseRoman("MCMLVII"))
+# calculate the result
+# TODO: set this up so it does not run if imported as a module
+print(parseRoman(input))
+
+# some tests
+#print(parseRoman("IV"))
+#print(parseRoman("XX"))
+#print(parseRoman("MCMLVII"))
